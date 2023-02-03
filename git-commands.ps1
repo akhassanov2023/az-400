7441d13c2842272b214bd41e95022d76a599c192
alex Khassanov <akhasanov@hotmail.com>
2023-02-03 14:21:04 -0500

    Commit message 1

--- git-commands.ps1
+++ git-commands.ps1
@@ -2,4 +2,9 @@ git version                     # show git version)
 git init                        # initialize empty git repo
 git config --global user.name "alex Khassanov"
 git config --global user.email "akhasanov@hotmail.com"
-git add .                      # add files from working directory to the staging area
+# Working Directory -> Staging Area
+git add .                       # add files from working directory to the staging area
+git status                      # current status fo the working directory
+# Staging Area -> Git Repo
+git commit -m "Commit message"  # add files from staging area to repo
+git status
