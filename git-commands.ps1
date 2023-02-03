210a22e9242645a07d844ced721006f5728d9be2
alex Khassanov <akhasanov@hotmail.com>
2023-02-03 14:12:39 -0500

    Commit message

---
+++ git-commands.ps1
@@ -0,0 +1,5 @@
+git version                     # show git version)
+git init                        # initialize empty git repo
+git config --global user.name "alex Khassanov"
+git config --global user.email "akhasanov@hotmail.com"
+git add .                      # add files from working directory to the staging area
